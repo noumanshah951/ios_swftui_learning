df0fa8cebc49b2e353e774ca6804832dea2d7970
nouman.shah <nouman.shah@visionx.io>
2020-10-25 23:38:23 +0500

    inital_commit

---
+++ ios_swftui_learning/ios_swftui_learningApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_swftui_learningApp.swift
+//  ios_swftui_learning
+//
+//  Created by Noman Shah on 25/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct ios_swftui_learningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
